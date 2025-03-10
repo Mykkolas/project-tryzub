579c3f873a1923164c269ac45df1283c6750c4b8
Anastasia <anastasiapanasian@gmail.com>
2025-03-10 18:07:52 +0300

    project-tryzub

---
+++ src/js/aboutme.js
@@ -0,0 +1,86 @@
+import Accordion from "accordion-js";
+import "accordion-js/dist/accordion.min.css";
+
+export function initializeAccordion() {
+  document.addEventListener("DOMContentLoaded", function () {
+    const accordion = new Accordion(".accordion", {
+      duration: 400,
+      showMultiple: false,
+      openOnInit: [0],
+      collapse: true,
+    });
+
+    setTimeout(() => {
+      const firstItem = document.querySelector(".accordion-item");
+      if (firstItem && !firstItem.classList.contains("is-open")) {
+        firstItem.classList.add("is-open");
+      }
+    }, 100);
+
+    const firstItem = document.querySelector(".accordion-item");
+    if (firstItem) {
+      if (!firstItem.classList.contains("is-open")) {
+        firstItem.classList.add("is-open");
+        
+        const content = firstItem.querySelector(".accordion-content");
+        if (content) {
+          content.style.height = content.scrollHeight + "px";
+        }
+      }
+    }
+
+    const accordionItems = document.querySelectorAll(".accordion-item");
+    accordionItems.forEach(item => {
+      const toggleButton = item.querySelector(".accordion-toggle");
+      
+      toggleButton.addEventListener("click", () => {
+        if (item.classList.contains("is-open")) {
+          item.classList.remove("is-open");
+          const content = item.querySelector(".accordion-content");
+          if (content) {
+            content.style.height = "0";
+          }
+        } else {
+          item.classList.add("is-open");
+          const content = item.querySelector(".accordion-content");
+          if (content) {
+            content.style.height = content.scrollHeight + "px";
+          }
+        }
+      });
+    });
+  });
+  
+}
+
+export function skillSwiper() {
+  document.addEventListener("DOMContentLoaded", function () {
+    skillSwiper();
+});
+
+function skillSwiper() {
+    const swiper = new Swiper(".skills-sld", {
+        slidesPerView: 1, // Динамічне оновлення
+        spaceBetween: 16,
+        loop: true, // Для безкінечної прокрутки
+        navigation: {
+            nextEl: ".swiper-btn-next",
+        },
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+        },
+        a11y: {
+          enabled: true, // Доступність
+      },
+        breakpoints: {
+            768: {
+                slidesPerView: 3,
+            },
+            1440: {
+                slidesPerView: 5,
+            },
+        }
+    });
+}
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 /* import js partials */
-
+import { initializeAccordion } from "./js/aboutme";
+initializeAccordion();
 
 import handleSubmit from "./js/footer";
 import fetchReviews from "./js/reviews";
