b548439057b1437f1d663deef57f3510a052eb5d
Mykola <ipadikys@gmail.com>
2025-03-10 21:41:04 +0100

    fixed swiper

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 /* import js partials */
-
-
 import handleSubmit from "./js/footer";
 import fetchReviews from "./js/reviews";
+/* import { initAccordion, initSwiper } from "./js/aboutme";
+
+initAccordion();
+initSwiper(); */
