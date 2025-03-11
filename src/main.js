0a3076d6214c2fa9645dc7c788e599a4326dcc0b
Anastasia <anastasiapanasian@gmail.com>
2025-03-11 12:29:43 +0300

    added faq

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 /* import js partials */
-<<<<<<< HEAD
 import { initializeAccordion } from "./js/aboutme";
 initializeAccordion();
 
 import { initializeFAQAccordion } from "./js/faq";
 
-=======
->>>>>>> 7a5c2ff9014f688e37a19ccc82cb4ab007c682cf
 import handleSubmit from "./js/footer";
 import fetchReviews from "./js/reviews";
 /* import { initAccordion, initSwiper } from "./js/aboutme";
