1389c1f9e60a8d21bb0f2d6ae8f3efda8dfd4d36
tochechka89 <rabota20ton@gmail.com>
2025-03-11 18:21:58 +0200

    header2.0:)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 /* import js partials */
+import "./js/header";
 import handleSubmit from "./js/footer";
 import fetchReviews from "./js/reviews";
+
 /* import { initAccordion, initSwiper } from "./js/aboutme";
 
 initAccordion();
